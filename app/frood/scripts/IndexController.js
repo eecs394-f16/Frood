977c82d3d3d39827209ca62c9816dbae5b7751c2
Nikhil <nikhofthehil@gmail.com>
2016-09-23 10:40:14 -0500

    Adding test event

--- app/frood/scripts/IndexController.js
+++ app/frood/scripts/IndexController.js
@@ -2,7 +2,12 @@ var app = angular.module('frood')
 
   app.controller('IndexController', function($scope, supersonic) {
 
-
-
+    $scope.testEvent = {
+        name: "GEECS BBQ",
+        loc: "The Lakefill",
+        time: "Sep 30, 6:00pm",
+        foodType: "BBQ",
+        desc: "Come join Graduate EECS for friends and food at the Lakefill"
+    }
     
   });
