066ae8816591ad46f4cde07db9d40b2ff3143c55
KevinXue2015 <xuecao.tju@gmail.com>
2016-09-23 11:02:44 -0500

    add tango event

--- app/frood/scripts/IndexController.js
+++ app/frood/scripts/IndexController.js
@@ -4,13 +4,21 @@ var app = angular.module('frood')
 <<<<<<< HEAD
 =======
 
-    $scope.testEvent = {
+    $scope.testEvent1 = {
         name: "GEECS BBQ",
         loc: "The Lakefill",
         time: "Sep 30, 6:00pm",
         foodType: "BBQ",
         desc: "Come join Graduate EECS for friends and food at the Lakefill"
     }
->>>>>>> 7ef392d7ab622d949602f3b406c7d060aa27bd60
+
     
+>>>>>>> 7ef392d7ab622d949602f3b406c7d060aa27bd60
+    $scope.testEvent2 = {
+        name: "NuTango Dance Lessons",
+        loc: " Parkes Hall 034, 1841 Chicago Avenue, Evanston",
+        time: "Wed, 8:45pm",
+        foodType: "Pizza",
+        desc: "Come on Wednesdays to meet awesome people and learn one of the most beautiful dances in the world."
+    }
   });
